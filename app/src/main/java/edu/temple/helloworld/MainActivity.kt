179365b4b82fef91dc270ba491d329fea51bca28
Viraj Shah <shahvirajd@gmail.com>
2021-09-16 00:29:48 -0400

    Added button which changes the greeting text.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,10 +2,15 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener { _ ->
+            findViewById<TextView>(R.id.greeting).text = "You clicked the button!"
+        }
     }
 }
